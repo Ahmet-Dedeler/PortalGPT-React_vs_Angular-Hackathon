442ea7fc6b5a3e71680dab25f985f896df4c31bc
Michael Del Pape <m4delpape@gmail.com>
2023-07-28 15:33:57 -0700

    Added canvas annimation

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,17 +2,19 @@ import React, { useState } from 'react';
 import '../styles.css';
 
 
-export default function Home({ setPage }) {
+export default function Home({ setPage, canvasRef }) {
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <div className="mb-8 text-2xl font-bold text-blue-500">
+    <div className="flex flex-col items-center justify-center h-screen bg-[#ebebeb]">
+      <canvas ref={canvasRef} className="absolute top-0 left-0 w-full h-full z-0">
+      </canvas>
+      <div className="mb-8 text-2xl font-bold text-blue-500 z-10">
         This is Home
       </div>
       <button onClick={() => {
         setPage('Chats');
       }}
-        className="px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-700">
+        className="px-4 py-2 text-white bg-blue-500 rounded hover:bg-blue-700 z-10">
         click to chat
       </button>
     </div>
