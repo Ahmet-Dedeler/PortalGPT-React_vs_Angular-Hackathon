64c4576d47c419b1550fdd24ae659511e442e7bd
Tony Truong <Tonytruong0698@gmail.com>
2023-07-28 15:34:41 -0700

    chat page structured

---
+++ client/src/components/Messages.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Messages() {
+
+  return (
+    <div className="h-5/6 w-5/6 border-black border-solid border-2 mb-3 rounded-md">
+    </div>
+  )
+}
